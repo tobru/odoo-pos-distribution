6e7188e6f8b86e2fcbf2954236146978c7fa24b2
Tobias Brunner <tobias@tobru.ch>
2019-07-06 13:14:05 +0200

    only print order number when ordered

--- odoo/addons/ip_pos_ticket_order_number/static/src/js/pos_ticket_order_number.js
+++ odoo/addons/ip_pos_ticket_order_number/static/src/js/pos_ticket_order_number.js
@@ -13,14 +13,22 @@ odoo.define('ip_pos_ticket_order_number.pos_ticket_order_number', function (requ
     Screens.ReceiptScreenWidget.include({
         get_receipt_render_env: function() {
             var recipt = this._super();
+            debugger;
             var number = recipt.order.name.split(' ')[1].split('-').pop()
             recipt['order_number'] = Number(number).toString();
+            recipt['big_number'] = recipt.order.big_number;
             return recipt;
         }
     });
 
     var _super_order = Models.Order.prototype;
     Models.Order = Models.Order.extend({
+        initialize: function() {
+            _super_order.initialize.apply(this,arguments);
+            if (typeof this.big_number === 'undefined') {
+                this.big_number = false;
+            } 
+        },
         computeChanges: function(categories){
             var res = _super_order.computeChanges.apply(this, categories)
             var number = res.name.split(' ')[1].split('-').pop()
@@ -28,12 +36,27 @@ odoo.define('ip_pos_ticket_order_number.pos_ticket_order_number', function (requ
             return res
             
         },
-        export_for_printing: function(){
-            var res = _super_order.export_for_printing.apply(this);
-            var number = this.name.split(' ')[1].split('-').pop()
-            res['order_number'] = Number(number).toString();
-            return res;
+        printChanges: function(){
+            var orders = _super_order.printChanges.call(this);
+            var order = this.pos.get_order()
+            debugger;
+            if (order) {
+                order.set_big_number(true);
+            }
+            return orders
+        },
+        set_big_number: function(val){
+            this.big_number = val;
+            this.trigger('change',this);
+        },
+        init_from_JSON: function(json) {
+            _super_order.init_from_JSON.apply(this,arguments);
+            this.big_number = json.big_number;
+        },
+        export_as_JSON: function() {
+            var json = _super_order.export_as_JSON.apply(this,arguments);
+            json.big_number = this.big_number;
+            return json;
         },
     });
-
 });
